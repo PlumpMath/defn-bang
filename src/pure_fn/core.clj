2fe75071dbba0bd78e0a0596eacaaa1f9d114a9d
Sebastian Bensusan <sbensu@gmail.com>
2015-11-27 23:29:04 +0100

    Add list impure functions from core

--- src/pure_fn/core.clj
+++ src/pure_fn/core.clj
@@ -1,4 +1,24 @@
-(ns pure-fn.core)
+(ns pure-fn.core
+  (:require [clojure.walk :as walk]))
+
+;; Try to determine if a function is pure or not, and make potential
+;; optimizations
+
+;; io -> do
+
+;; What about transient operations?
+
+;; What about implicit do in let
+
+(def impure-fns
+  '#{do dotimes dorun doall vswap! vreset! volatile!
+     pr prn print printf println doto spit run!})
+
+(defn impure? [form]
+  (walk/postwalk (fn [f]
+                   (println f)
+                   (contains? impure-fns f))
+                 (macroexpand form)))
 
 (defn foo
   "I don't do a whole lot."

076979195edaf06975f351ae9e8379250fb860ec
Sebastian Bensusan <sbensu@gmail.com>
2015-11-27 22:37:17 +0100

    Init commit

---
+++ src/pure_fn/core.clj
@@ -0,0 +1,6 @@
+(ns pure-fn.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/pure_fn/core_test.clj
@@ -0,0 +1,7 @@
+(ns pure-fn.core-test
+  (:require [clojure.test :refer :all]
+            [pure-fn.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

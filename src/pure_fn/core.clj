cb14fb857b9534224c1b138b1f3997fe0e59d94d
Sebastian Bensusan <sbensu@gmail.com>
2015-11-28 00:32:49 +0100

    Get all the symbols in a function def

--- src/pure_fn/core.clj
+++ src/pure_fn/core.clj
@@ -1,5 +1,6 @@
 (ns pure-fn.core
-  (:require [clojure.walk :as walk]))
+  (:require [clojure.walk :as walk]
+            [clojure.set :as set]))
 
 ;; Try to determine if a function is pure or not, and make potential
 ;; optimizations
@@ -14,11 +15,19 @@
   '#{do dotimes dorun doall vswap! vreset! volatile!
      pr prn print printf println doto spit run!})
 
+(defn all-symbols
+  "Returns all the tokens used by a function"
+  [form]
+  (loop [form form
+         acc #{}]
+    (if (seq form)
+      (if (coll? (first form))
+        (recur (rest form) (set/union acc (all-symbols (first form))))
+        (recur (rest form) (conj acc (first form))))
+      acc)))
+
 (defn impure? [form]
-  (walk/postwalk (fn [f]
-                   (println f)
-                   (contains? impure-fns f))
-                 (macroexpand form)))
+  (not (empty? (set/intersection impure-fns (all-symbols form)))))
 
 (defn foo
   "I don't do a whole lot."
